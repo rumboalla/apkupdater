bdd028acd98ca9a006892eebaed2453861953ea3
rumboalla <rumboalla@users.noreply.github.com>
2016-08-23 17:10:42 +0200

    Experimental theme support.

--- app/src/main/java/com/apkupdater/activity/SettingsActivity.java
+++ app/src/main/java/com/apkupdater/activity/SettingsActivity.java
@@ -3,15 +3,20 @@ package com.apkupdater.activity;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.support.annotation.MainThread;
 
 import com.apkupdater.R;
+import com.apkupdater.updater.UpdaterOptions;
 import com.apkupdater.util.AlarmUtil;
 
 import org.androidannotations.annotations.EActivity;
+
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -35,9 +40,34 @@ public class SettingsActivity
 			if (key.equals(getString(R.string.preferences_general_alarm_key))) {
 				AlarmUtil alarmUtil = new AlarmUtil(mContext);
 				alarmUtil.setAlarmFromOptions();
+			} else if (key.equals(getString(R.string.preferences_general_theme_key))) {
+				changeTheme();
 			}
 		}
 	};
+
+	@MainThread
+	private void changeTheme(
+	) {
+		SettingsActivity_.intent(this).flags(FLAG_ACTIVITY_CLEAR_TOP).start();
+
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private void setThemeFromOptions(
+	) {
+		UpdaterOptions options = new UpdaterOptions(mContext);
+		if(options.getTheme().equals(getString(R.string.theme_blue))) {
+			setTheme(R.style.PreferenceScreenThemeBlue);
+		} else if (options.getTheme().equals(getString(R.string.theme_dark))) {
+			setTheme(R.style.AppThemeDark);
+		} else if (options.getTheme().equals(getString(R.string.theme_light))) {
+			setTheme(R.style.AppThemeLight);
+		}else if (options.getTheme().equals(getString(R.string.theme_orange))) {
+			setTheme(R.style.PreferenceScreenThemeOrange);
+		}
+	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -45,14 +75,22 @@ public class SettingsActivity
 	public void onCreate(
 		Bundle savedInstanceState
 	) {
+		mContext = getBaseContext();
+		setThemeFromOptions();
+
 		super.onCreate(savedInstanceState);
 
-		mContext = getBaseContext();
 		PreferenceManager.getDefaultSharedPreferences(mContext).registerOnSharedPreferenceChangeListener(mChanges);
 		addPreferencesFromResource(R.xml.preferences);
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
