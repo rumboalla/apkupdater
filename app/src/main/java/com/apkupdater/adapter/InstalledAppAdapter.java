d36f51a09c36e5e3b07119afadde3d0858ad6c14
rumboalla <rumboalla@users.noreply.github.com>
2017-03-14 13:28:42 +0000

    Replacing list view for recycler view, to make the scrolling work in 2.3

--- app/src/main/java/com/apkupdater/adapter/InstalledAppAdapter.java
+++ app/src/main/java/com/apkupdater/adapter/InstalledAppAdapter.java
@@ -1,78 +1,184 @@
 package com.apkupdater.adapter;
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 
 import com.apkupdater.model.InstalledApp;
+import com.apkupdater.updater.UpdaterOptions;
 import com.apkupdater.view.InstalledAppView;
 import com.apkupdater.view.InstalledAppView_;
 
-import org.androidannotations.annotations.EBean;
-import org.androidannotations.annotations.RootContext;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-@EBean
 public class InstalledAppAdapter
-	extends ArrayAdapter<InstalledApp>
+	extends RecyclerView.Adapter<InstalledAppAdapter.InstalledAppViewHolder>
+	implements View.OnLongClickListener
 {
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	@RootContext
-	Context context;
+	private List<InstalledApp> mApps;
+	private Context mContext;
+	private RecyclerView mView;
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	InstalledAppAdapter(
-			Context context
+	public class InstalledAppViewHolder
+		extends RecyclerView.ViewHolder
+	{
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		private InstalledAppView mView;
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		InstalledAppViewHolder(
+			InstalledAppView view
+		) {
+			super(view);
+			mView = view;
+		}
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		public void bind(
+			InstalledApp app
+		) {
+			mView.bind(app);
+		}
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	public InstalledAppAdapter(
+		Context context,
+		RecyclerView view,
+		List<InstalledApp> apps
 	) {
-		super(context, 0);
+		mContext = context;
+		mView = view;
+		mApps = sort(apps);
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@Override
-	@NonNull
-	public View getView(
-		int position,
-		View convertView,
-		@NonNull ViewGroup parent
+	public InstalledAppViewHolder onCreateViewHolder(
+		ViewGroup parent,
+		int viewType
 	) {
-		InstalledAppView app;
+		InstalledAppView v = InstalledAppView_.build(mContext);
+		v.setLayoutParams(new RecyclerView.LayoutParams(
+			ViewGroup.LayoutParams.MATCH_PARENT,
+			ViewGroup.LayoutParams.WRAP_CONTENT
+		));
+		v.setOnLongClickListener(this);
+		return new InstalledAppViewHolder(v);
+	}
 
-		if (convertView == null) {
-			app = InstalledAppView_.build(context);
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	private List<InstalledApp> sort(
+		List<InstalledApp> items
+	) {
+		// Lists to hold both types of apps
+		List<InstalledApp> normal = new ArrayList<>();
+		List<InstalledApp> ignored = new ArrayList<>();
+
+		// Get the ignore list
+		UpdaterOptions options = new UpdaterOptions(mContext);
+		List<String> ignore_list = options.getIgnoreList();
+
+		// Iterate and buld the temp lists
+		for (InstalledApp i : items) {
+			if (ignore_list.contains(i.getPname())) {
+				ignored.add(i);
+			} else {
+				normal.add(i);
+			}
+		}
+
+		// Build comparator
+		Comparator<InstalledApp> comparator = new Comparator<InstalledApp>() {
+			@Override
+			public int compare(InstalledApp o1, InstalledApp o2) {
+				return o1.getName().compareToIgnoreCase(o2.getName());
+			}
+		};
+
+		// Sort them
+		Collections.sort(normal, comparator);
+		Collections.sort(ignored, comparator);
+
+		// Build final
+		List<InstalledApp> ordered = new ArrayList<>();
+		for (InstalledApp i : normal) {
+			ordered.add(i);
+		}
+
+		for (InstalledApp i : ignored) {
+			ordered.add(i);
+		}
+
+		return ordered;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public boolean onLongClick(
+		View view
+	) {
+		// Get the ignore list from the options
+		UpdaterOptions options = new UpdaterOptions(mContext);
+		List<String> ignore_list = options.getIgnoreList();
+
+		InstalledApp app = mApps.get(mView.getChildLayoutPosition(view));
+
+		// If it's on the ignore remove, otherwise add it
+		if (ignore_list.contains(app.getPname())) {
+			ignore_list.remove(app.getPname());
 		} else {
-			app = (InstalledAppView) convertView;
+			ignore_list.add(app.getPname());
 		}
 
-		app.bind(getItem(position));
+		// Update the list
+		options.setIgnoreList(ignore_list);
 
-		return app;
+		// Sort it
+		mApps = sort(mApps);
+
+		notifyDataSetChanged();
+
+		return true;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@Override
-	public void notifyDataSetChanged(
+	public void onBindViewHolder(
+		InstalledAppViewHolder holder,
+		int position
 	) {
-		super.notifyDataSetChanged();
+		holder.bind(mApps.get(position));
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	public InstalledApp[] getValues(
+	@Override
+	public int getItemCount(
 	) {
-		InstalledApp [] values = new InstalledApp[getCount()];
-		for (int i = 0; i < getCount(); i++) {
-			values[i] = getItem(i);
-		}
-		return values;
+		return mApps.size();
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -80,4 +186,3 @@ public class InstalledAppAdapter
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
--- app/src/main/java/com/apkupdater/fragment/InstalledAppFragment.java
+++ app/src/main/java/com/apkupdater/fragment/InstalledAppFragment.java
@@ -4,16 +4,16 @@ package com.apkupdater.fragment;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
-import android.widget.ListView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.apkupdater.R;
+import com.apkupdater.adapter.InstalledAppAdapter;
 import com.apkupdater.event.InstalledAppTitleChange;
 import com.apkupdater.event.UpdateInstalledAppsEvent;
 import com.apkupdater.model.InstalledApp;
-import com.apkupdater.updater.UpdaterOptions;
-import com.apkupdater.util.AnimationUtil;
 import com.apkupdater.util.GenericCallback;
-import com.apkupdater.R;
-import com.apkupdater.adapter.InstalledAppAdapter;
 import com.apkupdater.util.InstalledAppUtil;
 import com.apkupdater.util.MyBus;
 import com.squareup.otto.Subscribe;
@@ -21,13 +21,9 @@ import com.squareup.otto.Subscribe;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EFragment;
-import org.androidannotations.annotations.ItemLongClick;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -39,10 +35,7 @@ public class InstalledAppFragment
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@ViewById(R.id.list_view)
-	ListView mListView;
-
-	@Bean
-	InstalledAppAdapter mAdapter;
+	RecyclerView mRecyclerView;
 
 	@Bean
 	InstalledAppUtil mInstalledAppUtil;
@@ -54,37 +47,11 @@ public class InstalledAppFragment
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	@ItemLongClick(R.id.list_view)
-	void onUpdateClicked(
-		InstalledApp app
-	) {
-		// Get the ignore list from the options
-		UpdaterOptions options = new UpdaterOptions(getContext());
-		List<String> ignore_list = options.getIgnoreList();
-
-		// If it's on the ignore remove, otherwise add it
-		if (ignore_list.contains(app.getPname())) {
-			ignore_list.remove(app.getPname());
-		} else {
-			ignore_list.add(app.getPname());
-		}
-
-		// Update the list
-		options.setIgnoreList(ignore_list);
-
-		// Sort it
-		sort(mItems);
-
-		setListAdapter(mItems);
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	@AfterViews
 	void init(
 	) {
 		mBus.register(this);
-		ViewCompat.setNestedScrollingEnabled(mListView, true);
+		ViewCompat.setNestedScrollingEnabled(mRecyclerView, true);
 		updateInstalledApps(new UpdateInstalledAppsEvent());
 	}
 
@@ -113,70 +80,17 @@ public class InstalledAppFragment
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	private List<InstalledApp> sort(
-		List<InstalledApp> items
-	) {
-		// Lists to hold both types of apps
-		List<InstalledApp> normal = new ArrayList<>();
-		List<InstalledApp> ignored = new ArrayList<>();
-
-		// Get the ignore list
-		UpdaterOptions options = new UpdaterOptions(getContext());
-		List<String> ignore_list = options.getIgnoreList();
-
-		// Iterate and buld the temp lists
-		for (InstalledApp i : items) {
-			if (ignore_list.contains(i.getPname())) {
-				ignored.add(i);
-			} else {
-				normal.add(i);
-			}
-		}
-
-		// Build comparator
-		Comparator<InstalledApp> comparator = new Comparator<InstalledApp>() {
-			@Override
-			public int compare(InstalledApp o1, InstalledApp o2) {
-				return o1.getName().compareToIgnoreCase(o2.getName());
-			}
-		};
-
-		// Sort them
-		Collections.sort(normal, comparator);
-		Collections.sort(ignored, comparator);
-
-		// Build final
-		List<InstalledApp> ordered = new ArrayList<>();
-		for (InstalledApp i : normal) {
-			ordered.add(i);
-		}
-
-		for (InstalledApp i : ignored) {
-			ordered.add(i);
-		}
-
-		return ordered;
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	@UiThread(propagation = UiThread.Propagation.REUSE)
 	protected void setListAdapter(
 		List<InstalledApp> items
 	) {
-		if (mAdapter == null || mListView == null || mBus == null) {
+		if (mRecyclerView == null || mBus == null) {
 			return;
 		}
 
-		mAdapter.clear();
-		mListView.setAdapter(mAdapter);
-
-		AnimationUtil.startListAnimation(mListView);
-
-		for (InstalledApp i : sort(items)) { // addAll needs API level 11+
-			mAdapter.add(i);
-		}
-
+		mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+		mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+		mRecyclerView.setAdapter(new InstalledAppAdapter(getContext(), mRecyclerView, items));
 
 		mBus.post(new InstalledAppTitleChange(getString(R.string.tab_installed) + " (" + items.size() + ")"));
 	}
