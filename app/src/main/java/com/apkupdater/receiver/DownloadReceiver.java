c534ae3d4f47b6c4e8a13a621f1beacd29a0c0fe
rumboalla <rumboalla@users.noreply.github.com>
2017-06-17 13:25:41 +0100

    Fixed possible crash when installing file. Margins fixed.

--- app/src/main/java/com/apkupdater/receiver/DownloadReceiver.java
+++ app/src/main/java/com/apkupdater/receiver/DownloadReceiver.java
@@ -3,12 +3,17 @@ package com.apkupdater.receiver;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import android.app.DownloadManager;
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 
+import com.apkupdater.model.LogMessage;
+import com.apkupdater.util.LogUtil;
+
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EReceiver;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -17,6 +22,8 @@ import org.androidannotations.annotations.EReceiver;
 public class DownloadReceiver
     extends BroadcastReceiver
 {
+    @Bean
+    LogUtil mLog;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -41,22 +48,32 @@ public class DownloadReceiver
         final Context context,
         final long id
     ) {
-        DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        DownloadManager.Query query = new DownloadManager.Query();
-        query.setFilterById(id);
-        Cursor cursor = manager.query(query);
-        if (cursor.moveToFirst()) {
-            int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
-            String uri = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
-            //String mime = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE));
-            if (status == DownloadManager.STATUS_SUCCESSFUL && uri != null) {
-                Intent install = new Intent(Intent.ACTION_VIEW);
-                install.setDataAndType(Uri.parse(uri), "application/vnd.android.package-archive");
-                install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                context.startActivity(install);
+        try {
+            DownloadManager manager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            DownloadManager.Query query = new DownloadManager.Query();
+            query.setFilterById(id);
+            Cursor cursor = manager.query(query);
+            if (cursor.moveToFirst()) {
+                int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
+                String uri = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                //String mime = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_MEDIA_TYPE));
+                if (status == DownloadManager.STATUS_SUCCESSFUL && uri != null) {
+                    Intent install = new Intent(Intent.ACTION_VIEW);
+                    install.setDataAndType(Uri.parse(uri), "application/vnd.android.package-archive");
+                    install.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    try {
+                        context.startActivity(install);
+                    } catch (ActivityNotFoundException e) {
+                        install.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(install);
+                    }
+                }
             }
+            cursor.close();
+            throw new Exception("test");
+        } catch (Exception e) {
+            mLog.log("openDownloadFile", String.valueOf(e), LogMessage.SEVERITY_ERROR);
         }
-        cursor.close();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
