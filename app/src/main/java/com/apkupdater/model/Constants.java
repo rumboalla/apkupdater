7a04f8357d2ee60211356561763b426369906ece
rumboalla <rumboalla@gmail.com>
2017-07-04 15:32:46 +0100

    Automatic installer now has a notification with progress and final report.

--- app/src/main/java/com/apkupdater/model/Constants.java
+++ app/src/main/java/com/apkupdater/model/Constants.java
@@ -11,6 +11,9 @@ public class Constants
     static public final String DownloadManagerAction = "android.intent.action.DOWNLOAD_COMPLETE";
     static public final String FileProvider = PackageName + ".fileprovider";
     static public final String ApkMime = "application/vnd.android.package-archive";
+    static public final int UpdaterNotificationId = 42;
+    static public final int SelfUpdateNotificationId = 42 + 1;
+    static public final int AutomaticUpdateNotificationId = 42 + 2;
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
--- app/src/main/java/com/apkupdater/service/AutomaticInstallerService.java
+++ app/src/main/java/com/apkupdater/service/AutomaticInstallerService.java
@@ -3,10 +3,15 @@ package com.apkupdater.service;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.NotificationCompat;
 
+import com.apkupdater.R;
 import com.apkupdater.model.AppState;
-import com.apkupdater.model.DownloadInfo;
+import com.apkupdater.model.Constants;
 import com.apkupdater.model.LogMessage;
 import com.apkupdater.model.Update;
 import com.apkupdater.util.DownloadUtil;
@@ -22,6 +27,7 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EService;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -30,8 +36,6 @@ import java.util.List;
 public class AutomaticInstallerService
 	extends IntentService
 {
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	@Bean
@@ -46,6 +50,9 @@ public class AutomaticInstallerService
 	@Bean
 	LogUtil mLogger;
 
+	private int Status = 0;
+
+
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public AutomaticInstallerService(
@@ -55,12 +62,101 @@ public class AutomaticInstallerService
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    private void doAppNotification(
+        int current,
+        int max,
+        Update u
+    ) {
+        Context c = getApplicationContext();
+        NotificationCompat.Builder b = new NotificationCompat.Builder(getApplicationContext());
+
+        b.setContentTitle(String.format(c.getString(R.string.automatic_installer_progress_title), current, max));
+        b.setContentText(String.format(c.getString(R.string.automatic_installing), u.getPname()));
+        b.setSmallIcon(R.drawable.ic_update);
+        b.setAutoCancel(true);
+        b.setLargeIcon(BitmapFactory.decodeResource(c.getResources(), R.mipmap.ic_launcher));
+        b.setStyle(new NotificationCompat.BigTextStyle());
+
+        // Launch notification
+        NotificationManager m = (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
+        m.notify(Constants.AutomaticUpdateNotificationId, b.build());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void doErrorNotification(
+    ) {
+        Context c = getApplicationContext();
+        NotificationCompat.Builder b = new NotificationCompat.Builder(getApplicationContext());
+
+        b.setContentTitle(c.getString(R.string.automatic_installer_title));
+        b.setContentText(c.getString(R.string.automatic_error));
+        b.setSmallIcon(R.drawable.ic_update);
+        b.setAutoCancel(true);
+        b.setLargeIcon(BitmapFactory.decodeResource(c.getResources(), R.mipmap.ic_launcher));
+
+        // Launch notification
+        NotificationManager m = (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
+        m.notify(Constants.AutomaticUpdateNotificationId, b.build());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    private void doFinalNotification(
+        List<Update> success,
+        List<Update> failure
+    ) {
+        Context c = getApplicationContext();
+        NotificationCompat.Builder b = new NotificationCompat.Builder(getApplicationContext());
+
+        // Build success string
+        String s = "";
+        if (success.size() > 0) {
+            s = String.format(c.getString(R.string.automatic_successfully_installed), success.size());
+            for (Update u : success) {
+                s = s.concat(u.getPname() + ", ");
+            }
+            s = s.substring(0, s.length() - 2);
+            s = s.concat(".");
+        }
+
+        // Build failure string
+        String f = "";
+        if (failure.size() > 0) {
+            f = String.format(c.getString(R.string.automatic_failed_to_install), failure.size());
+            for (Update u : failure) {
+                f = f.concat(u.getPname() + ", ");
+            }
+            f = f.substring(0, f.length() - 2);
+            f += ".";
+        }
+
+        b.setContentTitle(c.getString(R.string.automatic_installer_title));
+        b.setContentText(String.format("%s\n%s", s, f));
+        b.setSmallIcon(R.drawable.ic_update);
+        b.setAutoCancel(true);
+        b.setLargeIcon(BitmapFactory.decodeResource(c.getResources(), R.mipmap.ic_launcher));
+        b.setStyle(new NotificationCompat.BigTextStyle());
+
+        // Launch notification
+        NotificationManager m = (NotificationManager) c.getSystemService(Context.NOTIFICATION_SERVICE);
+        m.notify(Constants.AutomaticUpdateNotificationId, b.build());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	private void installApps(
 	    Update [] updates
     ) {
 	    try {
+	        List<Update> success = new ArrayList<>(), failure = new ArrayList<>();
+	        int i = 0;
+
             for (Update u : updates) {
                 try {
+                    i++;
+                    doAppNotification(i, updates.length + 1, u);
+
                     AndroidAppDeliveryData data = GooglePlayUtil.getAppDeliveryData(
                         GooglePlayUtil.getApi(getApplicationContext()),
                         u.getPname()
@@ -74,22 +170,31 @@ public class AutomaticInstallerService
 
                     if (f != null) {
                         if (FileUtil.installApk(f.getAbsolutePath())) {
-                            mLogger.log("installApps", "Installed " + u.getPname(), LogMessage.SEVERITY_INFO);
+                            if (InstalledAppUtil.getAppVersionCode(this, u.getPname()) == u.getNewVersionCode()) {
+                                success.add(u);
+                                mLogger.log("installApps", "Installed " + u.getPname(), LogMessage.SEVERITY_INFO);
+                            } else {
+                                failure.add(u);
+                                mLogger.log("installApps", "Failed to install " + u.getPname(), LogMessage.SEVERITY_INFO);
+                            }
                         } else {
+                            failure.add(u);
                             mLogger.log("installApps", "Failed to install " + u.getPname(), LogMessage.SEVERITY_INFO);
                         }
 
-                        // TODO: Should check with the package manager if app is really installed.
-
                         f.delete();
                     } else {
+                        failure.add(u);
                         mLogger.log("installApps", "Error downloading " + u.getPname(), LogMessage.SEVERITY_INFO);
                     }
                 } catch (Exception e) {
+                    failure.add(u);
                     mLogger.log("installApps " + u.getPname(), String.valueOf(e), LogMessage.SEVERITY_ERROR);
                 }
             }
+            doFinalNotification(success, failure);
         } catch (Exception e) {
+	        doErrorNotification();
 	        mLogger.log("installApps", String.valueOf(e), LogMessage.SEVERITY_ERROR);
         }
     }
