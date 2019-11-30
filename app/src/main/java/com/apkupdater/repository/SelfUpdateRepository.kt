64e868655cf879c22586f2600ac517e0b3aa88bc
rumboalla <rumboalla@users.noreply.github.com>
2019-11-30 19:07:09 +0100

    Improving self update.

--- app/src/main/java/com/apkupdater/repository/SelfUpdateRepository.kt
+++ app/src/main/java/com/apkupdater/repository/SelfUpdateRepository.kt
@@ -23,25 +23,26 @@ class SelfUpdateRepository: KoinComponent {
 	private val prefs: AppPreferences by inject()
 
 	private val url = "http://rumboalla.github.io/apkupdater/version.json"
-	private val interval = 24 * 60 * 60 * 1000
+	private val interval = 60 * 60 * 1000
 
 	fun checkForUpdatesAsync(activity: Activity) = ioScope.catchingAsync {
 		if (System.currentTimeMillis() - prefs.selfUpdateCheck() > interval) {
 			val response = Fuel.get(url).responseObject<SelfUpdateResponse>().third.get()
-			prefs.selfUpdateCheck(System.currentTimeMillis())
 			if (response.version > activity.packageManager.getPackageInfo(activity.packageName, 0).versionCode) {
-				if (withContext(Dispatchers.Main) { showDialog(activity) }) {
+				if (withContext(Dispatchers.Main) { showDialog(activity, response) }) {
+					prefs.selfUpdateCheck(System.currentTimeMillis())
 					installer.install(activity, installer.downloadAsync2(activity, response.apk) { _, _ -> }, 0)
 				}
 			}
 		}
 	}
 
-	private suspend fun showDialog(activity: Activity) = suspendCoroutine<Boolean> {
+	private suspend fun showDialog(activity: Activity, response: SelfUpdateResponse) = suspendCoroutine<Boolean> {
 		AlertDialog.Builder(activity, R.style.AlertDialogTheme)
-			.setMessage(activity.getString(R.string.selfupdate_found))
-			.setPositiveButton(activity.getString(R.string.selfupdate_ok)) { _, _: Int -> it.resume(true) }
-			.setNegativeButton(activity.getString(R.string.selfupdate_cancel)) { _, _ -> it.resume(false) }
+			.setTitle(activity.getString(R.string.selfupdate_found_title))
+			.setMessage(response.changelog)
+			.setPositiveButton(activity.getString(R.string.action_install)) { _, _: Int -> it.resume(true) }
+			.setNegativeButton(activity.getString(android.R.string.cancel)) { _, _ -> it.resume(false) }
 			.create()
 			.show()
 	}
