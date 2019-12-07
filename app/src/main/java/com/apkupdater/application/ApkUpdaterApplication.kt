f7c5624d511f3c376ebba6671a5fc9183d2bb5e2
rumboalla <rumboalla@users.noreply.github.com>
2019-12-07 14:01:56 +0100

    Added Acra crash report.

--- app/src/main/java/com/apkupdater/application/ApkUpdaterApplication.kt
+++ app/src/main/java/com/apkupdater/application/ApkUpdaterApplication.kt
@@ -2,20 +2,31 @@ package com.apkupdater.application
 
 import android.app.Application
 import com.apkupdater.di.mainModule
+import org.acra.ACRA
+import org.acra.BuildConfig
+import org.acra.annotation.AcraCore
+import org.acra.annotation.AcraHttpSender
+import org.acra.sender.HttpSender
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
+@AcraCore(buildConfigClass = BuildConfig::class)
+@AcraHttpSender(httpMethod = HttpSender.Method.POST, uri = "https://collector.tracepot.com/8ead3e03")
 class ApkUpdaterApplication : Application() {
 
 	override fun onCreate() {
 		super.onCreate()
-
-		startKoin{
-			androidLogger()
-			androidContext(this@ApkUpdaterApplication)
-			modules(mainModule)
-		}
+		initAcra()
+		initKoin()
 	}
+
+	private fun initKoin() = startKoin{
+		androidLogger()
+		androidContext(this@ApkUpdaterApplication)
+		modules(mainModule)
+	}
+
+	private fun initAcra() = ACRA.init(this)
 
 }
