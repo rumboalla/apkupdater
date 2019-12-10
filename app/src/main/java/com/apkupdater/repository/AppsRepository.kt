a38d3c2340f144993f47ef86c630bd660e9c4d38
rumboalla <rumboalla@users.noreply.github.com>
2019-12-10 10:43:45 +0100

    Catching bug with package manager.

--- app/src/main/java/com/apkupdater/repository/AppsRepository.kt
+++ app/src/main/java/com/apkupdater/repository/AppsRepository.kt
@@ -3,6 +3,7 @@ package com.apkupdater.repository
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
+import android.util.Log
 import com.apkupdater.model.ui.AppInstalled
 import com.apkupdater.util.app.AppPrefs
 import com.apkupdater.util.name
@@ -14,10 +15,15 @@ class AppsRepository(private val context: Context, private val prefs: AppPrefs) 
 	private val excludeDisabled get() = prefs.settings.excludeDisabled
 	private val ignoredApps get() = prefs.ignoredApps()
 
-	private fun getPackageInfos(options: Int = 0) = context.packageManager.getInstalledPackages(options).asSequence()
-		.filter { !excludeSystem || it.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
-		.filter { !excludeSystem || it.applicationInfo.flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP == 0 }
-		.filter { !excludeDisabled || it.applicationInfo.enabled }
+	private fun getPackageInfos(options: Int = 0): Sequence<PackageInfo> = runCatching {
+		return context.packageManager.getInstalledPackages(options).asSequence()
+			.filter { !excludeSystem || it.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0 }
+			.filter { !excludeSystem || it.applicationInfo.flags and ApplicationInfo.FLAG_UPDATED_SYSTEM_APP == 0 }
+			.filter { !excludeDisabled || it.applicationInfo.enabled }
+	}.getOrElse {
+		Log.e("AppsRepository", "getPackageInfos", it)
+		return sequenceOf()
+	}
 
 	fun getPackageInfosFiltered(options: Int = 0) = getPackageInfos(options).filter { !ignoredApps.contains(it.packageName) }
 
