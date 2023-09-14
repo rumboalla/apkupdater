ce906d40334ef35fd2a9638ad0e1fb219a35a466
kyawlinnthant <kyawlinnthantkyawlinnthant@gmail.com>
2023-09-14 14:47:16 +0630

    update gradle build to version catalog
    updated build logic to version catalog with bundle, plugin, libraries, versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import java.io.FileInputStream
 import com.android.build.gradle.internal.api.BaseVariantOutputImpl
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.kotlin)
+    alias(libs.plugins.android.application)
 }
 
 android {
@@ -46,7 +46,10 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -62,9 +65,9 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = JavaVersion.VERSION_1_8.toString() }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    composeOptions { kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get() }
 
     packaging {
         resources {
@@ -79,34 +82,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
-    implementation("androidx.compose.ui:ui:1.5.0")
-    implementation("androidx.tv:tv-foundation:1.0.0-alpha08")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("io.insert-koin:koin-android:3.4.2")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.2")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs:0.4.3")
-    implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs-gson:0.4.3")
-    implementation("org.jsoup:jsoup:1.16.1")
-    implementation("eu.chainfire:libsuperuser:1.1.1")
-    implementation("io.github.g00fy2:versioncompare:1.5.0")
-
-    testImplementation("junit:junit:4.13.2")
-
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.0")
-
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.0")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.0")
-
+    implementation(libs.bundles.androidx.core)
+    implementation(libs.bundles.di)
+    implementation(libs.bundles.network)
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.jsoup)
+    implementation(libs.kryptoprefs)
+    implementation(libs.kryptoprefs.gson)
+    implementation(libs.libsuperuser)
+    implementation(libs.versioncompare)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.junit.ext)
+    androidTestImplementation(libs.espresso)
+    androidTestImplementation(libs.compose.test)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.android.kotlin).apply(false)
+    alias(libs.plugins.android.application).apply(false)
 }
