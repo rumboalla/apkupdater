250b28f5c851e0718c4c31574636dcfa67a62c9e
Zzzz <46766062+samuraikid0@users.noreply.github.com>
2023-09-30 13:52:03 -1100

    More 3 apps

--- app/src/main/kotlin/com/apkupdater/data/github/GitHubApp.kt
+++ app/src/main/kotlin/com/apkupdater/data/github/GitHubApp.kt
@@ -89,7 +89,10 @@ val GitHubApps = listOf(
     GitHubApp("com.bnyro.translate", "you-apps", "TranslateYou"),
     GitHubApp("com.bnyro.recorder", "you-apps", "RecordYou"),
     GitHubApp("keepass2android.keepass2android_nonet", "PhilippC", "keepass2android", contains("nonet")),
-    GitHubApp("keepass2android.keepass2android", "PhilippC", "keepass2android", notContains("nonet"))
+    GitHubApp("keepass2android.keepass2android", "PhilippC", "keepass2android", notContains("nonet")),
+    GitHubApp("com.fsck.k9", "thundernest", "k-9"),
+    GitHubApp("eu.darken.sdmse", "d4rken-org", "sdmaid"),
+    GitHubApp("oss.krtirtho.spotube", "KRTirtho", "spotube")
 )
 
 fun contains(text: String) = Regex("^.*$text.*\$")
