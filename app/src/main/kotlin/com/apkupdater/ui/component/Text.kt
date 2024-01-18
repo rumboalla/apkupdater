5bc7262748e99dbed24ba00353cc1f78cba9b19f
Horizon <64575533+BlueGradientHorizon@users.noreply.github.com>
2024-01-18 23:24:43 +0300

    Update Text.kt: fix imports

--- app/src/main/kotlin/com/apkupdater/ui/component/Text.kt
+++ app/src/main/kotlin/com/apkupdater/ui/component/Text.kt
@@ -1,5 +1,6 @@
 package com.apkupdater.ui.component
 
+import android.provider.Settings
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
