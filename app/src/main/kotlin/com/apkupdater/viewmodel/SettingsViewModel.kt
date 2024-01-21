231c461b068a54c2ae2fac076f5b274205633baf
bluegradienthorizon <safana53656@gmail.com>
2024-01-21 11:28:17 +0300

    New settings option "Play text animations" + russian translation updates

--- app/src/main/kotlin/com/apkupdater/viewmodel/SettingsViewModel.kt
+++ app/src/main/kotlin/com/apkupdater/viewmodel/SettingsViewModel.kt
@@ -36,6 +36,8 @@ class SettingsViewModel(
 	fun getPortraitColumns() = prefs.portraitColumns.get()
 	fun setLandscapeColumns(n: Int) = prefs.landscapeColumns.put(n)
 	fun getLandscapeColumns() = prefs.landscapeColumns.get()
+	fun setPlayTextAnimations(b: Boolean) = prefs.playTextAnimations.put(b)
+	fun getPlayTextAnimations() = prefs.playTextAnimations.get()
 	fun setIgnoreAlpha(b: Boolean) = prefs.ignoreAlpha.put(b)
 	fun getIgnoreAlpha() = prefs.ignoreAlpha.get()
 	fun setIgnoreBeta(b: Boolean) = prefs.ignoreBeta.put(b)
