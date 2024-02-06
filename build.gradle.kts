6ca13692490fccbe02af29143d4343dac0c12398
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-02-06 17:31:36 +0100

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
