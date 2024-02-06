6df06886bed97c497ccb83e34de0ad65ab650829
Jvr <109031036+Jvr2022@users.noreply.github.com>
2024-02-06 18:22:23 +0100

    bump deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ android {
 
     kotlinOptions { jvmTarget = "1.8" }
     buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.8" }
 
     packaging {
         resources {
@@ -80,33 +80,33 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.compose.ui:ui:1.5.1")
-    implementation("androidx.tv:tv-foundation:1.0.0-alpha09")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("io.insert-koin:koin-android:3.4.2")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.compose.ui:ui:1.6.0")
+    implementation("androidx.tv:tv-foundation:1.0.0-alpha10")
+    implementation("androidx.compose.material3:material3:1.2.0-rc01")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation("io.insert-koin:koin-androidx-compose:3.5.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.5.0")
     implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs:0.4.3")
     implementation("com.github.rumboalla.KryptoPrefs:kryptoprefs-gson:0.4.3")
-    implementation("org.jsoup:jsoup:1.16.1")
-    implementation("com.github.topjohnwu.libsu:core:5.2.1")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("com.github.topjohnwu.libsu:core:5.2.2")
     implementation("io.github.g00fy2:versioncompare:1.5.0")
 
     testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
 
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.0")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.6.0")
 
 }
